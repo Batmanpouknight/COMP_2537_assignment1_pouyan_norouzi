6811014381c13a4d080565eef547f2f4703b0a15
Pouyan Norouzi <irfn7pouyan@gmail.com>
2024-05-01 00:18:21 -0700

    Created html files for pages, added a somewhat functional signup page

--- index.js
+++ index.js
@@ -1,15 +1,107 @@
+require("./utils.js");
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
 const app = express();
-const port = 3000;
+const port = 3005;
+
+const Joi = require("joi");
+
+const expireTime = 24 * 60 * 60 * 1000;
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+var { database } = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+app.use(express.urlencoded({ extended: false }));
+
+var mongoStore = MongoStore.create({
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+    crypto: {
+        secret: mongodb_session_secret
+    }
+})
+
+app.use(session({
+    secret: node_session_secret,
+    store: mongoStore, //default is memory store 
+    saveUninitialized: false,
+    resave: true
+}
+));
 
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/html', express.static(__dirname + '/public/html'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/html/home.html');
-
 });
+app.get('/home2', (req, res) => {
+    res.sendFile(__dirname + '/public/html/homeLoggedIn.html');
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(__dirname + '/public/html/signup.html');
+});
+
+app.post('/signupSubmit', async (req, res) => {
+    var username = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    const schema = Joi.object(
+        {
+            username: Joi.string().alphanum().max(20).required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().max(20).required()
+        });
+
+    const validationResult = schema.validate({ username, email, password });
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect("/signup");
+        return;
+    }
+
+    var hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    await userCollection.insertOne({username: username, email: email, password: hashedPassword});
+	console.log("Inserted user");
+
+    res.redirect("/members");
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/html/login.html');
+});
+
+app.get('/members', (req, res) => {
+    res.sendFile(__dirname + '/public/html/members.html');
+});
+
+app.get('/logout', (req, res) => {
+    // Perform logout logic here
+    res.redirect('/');
+});
+
+app.get("*", (req, res) => {
+    res.status(404);
+    res.send("Page not found - 404");
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
